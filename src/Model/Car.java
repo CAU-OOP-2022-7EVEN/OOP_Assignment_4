7d1c92e1ce56c468c0865a7ebf413114aeec0510
yymin1022 <yymin1022@gmail.com>
2022-11-28 13:23:38 +0900

    Update Car : Added Base Variables

--- src/Model/Car.java
+++ src/Model/Car.java
@@ -1,5 +1,11 @@
 package Model;
 
 public class Car{
-
+    private String manufacture;
+    private String name;
+    private String number;
+    private int capacity;
+    private int price;
+    private int year;
+    private boolean isAccident;
 }
