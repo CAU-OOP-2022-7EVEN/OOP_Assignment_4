c7a856ee81944ad718657231de691274cd057bc6
yymin1022 <yymin1022@gmail.com>
2022-11-28 14:12:48 +0900

    Update Accident : Added Class Constructor

--- src/Model/Accident.java
+++ src/Model/Accident.java
@@ -3,4 +3,9 @@ package Model;
 public class Accident{
     String date;
     String content;
+
+    public Accident(String date, String content){
+        this.date = date;
+        this.content = content;
+    }
 }
