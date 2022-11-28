fdb6fe69e60845483405e9c5c1119d54ec2ce270
yymin1022 <yymin1022@gmail.com>
2022-11-28 13:25:52 +0900

    Update Car : Added Constructor

--- src/Model/Car.java
+++ src/Model/Car.java
@@ -1,11 +1,21 @@
 package Model;
 
 public class Car{
-    private String manufacture;
     private String name;
+    private String manufacture;
     private String number;
-    private int capacity;
     private int price;
+    private int capacity;
     private int year;
     private boolean isAccident;
+
+    public Car(String name, String manufacture, String number, int price, int capacity, int year, boolean isAccident){
+        this.name = name;
+        this.manufacture = manufacture;
+        this.number = number;
+        this.price = price;
+        this.capacity = capacity;
+        this.year = year;
+        this.isAccident = isAccident;
+    }
 }
